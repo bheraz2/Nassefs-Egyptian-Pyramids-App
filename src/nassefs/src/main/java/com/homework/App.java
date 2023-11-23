fa9fed3d0241d13c755a1e3dbaf4bc833d1fe854
Bryan Heraz <bherazbackup2@gmail.com>
2023-11-22 16:57:07 -0800

    I created a method for printMenu and printMenuCommand

--- src/nassefs/src/main/java/com/homework/App.java
+++ src/nassefs/src/main/java/com/homework/App.java
@@ -1,13 +1,34 @@
 package com.homework;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+import java.util.*;
+import org.json.simple.*;
+
 public class App 
 {
-    public static void main( String[] args )
+    /*public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }*/
+
+    public static void printMenu(){
+       System.out.println("----------------------------------------------------------");
+       System.out.println("Nassef's Egyptian Pyramids App");
+       System.out.println("----------------------------------------------------------");
+       System.out.println("Command\tDescription");
+       System.out.println("-------\t----------------------------------");
+       printMenuCommand('1',"List of all pharaohs");
+       printMenuCommand('2',"Display a specific Egyptian pharaoh");
+       printMenuCommand('3',"List of all pyramids");
+       printMenuCommand('4',"Display a specific pyramid");
+       printMenuCommand('5',"Displays a list of requested pyramids");
+       printMenuCommand('q',"Quit");
+
+
+
+    }
+
+    public static void printMenuCommand(Character command, String desc){
+        System.out.printf("%s\t%s\n",command, desc);
     }
 }
